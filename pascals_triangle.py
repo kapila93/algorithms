dbd416ee8d855165bcac6993e82c95d4ffcce57e
Kapila Ramji <kapilaramji@yahoo.com>
2015-09-24 20:05:59 +0600

    pascal's triangle completed

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -4,33 +4,35 @@ def pascal(power):
 	row = [1]
 	pwr_count = 1
 	print row
-	print ""
 	pascalrow(row, pwr_count, power)
 
-def pascalrow(some_row, pwr_count, power):
+def pascalrow(row, pwr_count, power):
 	if pwr_count < power:
-		print "setting new row to original row now"
-		new_row = some_row
+		# print "setting new row to original row now"
+		new_row = list(row)
 		
 		for index in range(0, pwr_count):
-			print "Loop # %s" % index
-			print "original row %s" % some_row
+			# print "Loop # %s" % index
+			# print "original row %s" % row
 			if index == 0:
-				print "appended 1"
+				# print "appended 1"
 				new_row.append(1)
 			elif index > 0 and index < len(new_row):
-				new_row[index] = some_row[index-1] + some_row[index]
-			print "New Row %s" % new_row
-			print "End of Loop # %s" % index
-			print ""
+				new_row[index] = row[index-1] + row[index]
+			else:
+				print "Nothing happened."
+			# print "New Row %s" % new_row
+			# print "End of Loop # %s" % index
+			# print ""
 
-		print "after the loop %s" % new_row
-		print ""
+		# print "after the loop %s" % new_row
+		print new_row
+		# print ""
 		pwr_count = pwr_count + 1
 		pascalrow(new_row, pwr_count, power)
 
 
-pascal(5)
+pascal(10)
 
 
 # 0 = 0
