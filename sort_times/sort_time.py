d79f228e0253a93b25910229c57a2b6000b0d151
Kapila Ramji <kapilaramji@yahoo.com>
2015-10-01 21:44:16 -0400

    optimized the code a little

--- sort_times/sort_time.py
+++ sort_times/sort_time.py
@@ -6,23 +6,18 @@ import random
 import time
 import csv
 
-# generate a list of pseudorandom numbers of length n. Adds
-def genList(length):
-	new_list = []
-	for i in range(length):
-		new_list.append(random.randint(0, 10000))
-	return new_list
-
  # takes a list, sorts using insert sort, then using merge sort and returns a list with the respective run times
 def sortAndTimeList(some_list):
-	original_list = list(some_list)
+	#insert sort
 	start_time = time.time()
-	insertSort(some_list)
+	insertSort(list(some_list))
 	insert_sort_time = round(((time.time() - start_time) * 1000), 4) # converts to milliseconds and rounds to 4 decimals
-	some_list = list(original_list)
+
+	#merge sort
 	start_time = time.time()
-	mergeSort(some_list)
+	mergeSort(list(some_list))
 	merge_sort_time = round(((time.time() - start_time) * 1000), 4) # converts to milliseconds and rounds to 4 decimals
+
 	times = [insert_sort_time, merge_sort_time]
 	return times
 
@@ -43,12 +38,16 @@ def writeToHTML(data_insert, data_merge):
 	writer.close()
 
 def createData():
-	data = {}
-	data_insert = ""
-	data_merge = ""
-	for i in range(0,16):
-		length = 2**i
-		times = sortAndTimeList(genList(length)) # 2^i done till 2^16th
+	master_list = []
+	data = {} # this data is for csv
+	data_insert = "" # this data is to write insert time to html
+	data_merge = "" # this data is to write merge time to html
+
+	for i in range(0,65000):
+		master_list.append(random.randint(-100, 100))
+		unsorted_list = list(master_list)
+		length = len(unsorted_list)
+		times = sortAndTimeList(unsorted_list) # 2^i done till 2^16th
 		if i == 0:
 			data_insert =  ("{ x: %s, y: %s }" % (length, times[0]))
 			data_merge = ("{ x: %s, y: %s }" % (length, times[1]))
@@ -57,6 +56,7 @@ def createData():
 			data_merge = data_merge + (",{ x: %s, y: %s }" % (length, times[1]))
 
 		data[length] = times[0], times[1] # appends sorting times for 2^i elements
+		print "%s completed" % i
 	writeToHTML(data_insert, data_merge)
 	writeToCSV(data)
 
